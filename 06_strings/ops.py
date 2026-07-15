98f92c0b79ecced95d594eb8fa1e20fdbc25ba76
ravi2krishna <ravi2krishna@gmail.com>
2026-07-15 10:59:59 +0530

    class 14 - String Methods Completed

--- 06_strings/ops.py
+++ 06_strings/ops.py
@@ -68,3 +68,91 @@ else:
     print("Pan Should Be 10 Characters Exactly")
     
 
+# Simulate Phone ISD Scenario 
+# https://us1.discourse-cdn.com/flex016/uploads/weweb/original/2X/d/dbe25afb4aeb05640347e2f7c1b7ae532ebb28f2.png
+# https://www.businessbloomer.com/wp-content/uploads/2014/11/woocommerce-add-coupon-automatically-to-cart-if-product.png
+
+# startswith() used to check if a string starts with a specific substring
+# endswith() used to check if a string ends with a specific substring
+
+
+
+# Simulate Data Operations Work: CSV Data from a file and perform some operations 
+# https://www.datablist.com/learn_images/csv/google_sheet_csv.png
+# https://www.slashgear.com/img/gallery/csv-files-explained-what-they-are-and-how-to-open-them/what-are-csv-files-1699455969.jpg
+
+
+# Name,Email,Age,City,Job_Role
+# emp_data = "John,john@apple.com,30,Hyderabad,Developer"
+
+# Requirement: Display Employee Name & Job Role
+
+emp_data = "John,john@apple.com,30,Hyderabad,Developer"
+emp_name = emp_data[0]
+print('Employee Name: ',emp_name)
+
+emp_name = emp_data[0:4]
+print('Employee Name: ',emp_name)
+
+emp_data = "Michael,michael@apple.com,40,Hyderabad,Admin"
+emp_name = emp_data[0:7]
+print('Employee Name: ',emp_name)
+
+# Above Logic is Hard Coded, which is not good in Dynamic Form 
+# split(): method divides a string into a list of substrings based
+# on a specified delimiter. By default, it splits the string by any consecutive whitespace
+
+emp_data = "Michael,michael@apple.com,40,Hyderabad,Admin"
+data_splitted = emp_data.split()
+print(data_splitted)
+
+emp_data = "Michael michael@apple.com 40 Hyderabad Admin"
+data_splitted = emp_data.split()
+print(data_splitted)
+
+emp_data = "Michael,michael@apple.com,40,Hyderabad,Admin"
+data_splitted = emp_data.split(",")
+print(data_splitted)
+print(type(data_splitted))
+
+# List also works on Index Basis, Same as Strings 
+
+emp_name = data_splitted[0]
+emp_role = data_splitted[-1]
+print('Employee Name: ',emp_name)
+print('Employee Role: ',emp_role)
+
+emp_data = "John,john@apple.com,30,Hyderabad,Developer"
+data_splitted = emp_data.split(",")
+emp_name = data_splitted[0]
+emp_role = data_splitted[-1]
+print('Employee Name: ',emp_name)
+print('Employee Role: ',emp_role)
+
+# Simulate Amazon Order Email / SMS / OTP Confirmation Template 
+
+order_template = "Hello, Your Order with order_id has been shipped"
+order_ids = "AMAZON-ID-1010029202,AMAZON-ID-8090029202,AMAZON-ID-9090029202,AMAZON-ID-7080029202"
+order_ids_splitted = order_ids.split(",")
+
+print(order_ids)
+print(order_ids_splitted)
+
+for order_id in order_ids_splitted:
+    print(order_id)
+
+order_template = "Hello, Your Order with order_id has been shipped"
+order_ids = "AMAZON-ID-1010029202,AMAZON-ID-8090029202,AMAZON-ID-9090029202,AMAZON-ID-7080029202"
+    
+# replace() method is used to swap out a specific substring for a new one within a string
+for order_id in order_ids_splitted:
+    # print(order_id)
+    send_email = order_template.replace("order_id",order_id)
+    print(send_email)
+    
+# Reset Password 
+# old_password -> 
+# new_password ->
+# reenter_new_password
+
+# if old_password == "Admin123" and new_password == "Login123" and reenter_new_password == "Login123"

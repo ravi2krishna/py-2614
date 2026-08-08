3c4904d598902652e6bd980d36f2a8b69fca7edc
ravi2krishna <ravi2krishna@gmail.com>
2026-08-08 09:32:38 +0530

    Class - 31 - OOP Introduction

---
+++ 16_oops/demo.py
@@ -0,0 +1,16 @@
+# OOP 
+
+# Class - Blue Print
+
+class Student: # CLASS 
+    
+    # Student HAS Something - Characteristics / Properties (Variables)
+    student_name = "Ravi" # VARIABLE
+    student_email = "ravi2krishna@gmail.com"
+    
+    # Student DOES Something - Behaviors / Actions (Methods)
+    def student_studies(self): # METHOD
+        print("Student Is Studying Python")
+        
+# To Use This Class, We Need Object 
+student_object = Student() # OBJECT
